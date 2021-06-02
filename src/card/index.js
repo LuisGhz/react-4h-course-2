a79de7cd1d1a59a9a4e92232a3f7ca80e900a42f
Luis Gutiérrez <luisghtz@outlook.com>
2021-06-02 14:24:01 -0500

    Fix red button class

--- src/card/index.js
+++ src/card/index.js
@@ -29,7 +29,7 @@ export const Card = props => {
         <h4><b>{ props.name }</b></h4>
         <p>{ props.phone }</p>
         <p><input type="text" value={ props.name } onChange={ props.onNameChanges } /></p>
-        <p className="center-text"><button className="button button-red" onClick={ () => props.onDelete() } >Delete</button></p>
+        <p className="center-text"><button className="button red-button" onClick={ () => props.onDelete() } >Delete</button></p>
         <div>{ props.children }</div>
       </div>
     </div>
