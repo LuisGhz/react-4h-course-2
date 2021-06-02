c8de8bd52c3ca8a560595afab21acfed34b4f153
Luis Gutiérrez <luisghtz@outlook.com>
2021-06-01 20:52:17 -0500

    Change default content by first project content

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { ThemeProvider } from 'styled-components';
+import { Card } from 'card/index';
+import Button from 'elements/button/Button';
 import './App.css';
 
+const theme = {
+  primary: 'green',
+  mango: 'yellow'
+}
+
 function App() {
+  const [showCard, setShowCard] = useState(true);
+
+  const [cards, setCards] = useState([
+    {
+      avatar: 'https://cdn.fakercloud.com/avatars/mwarkentin_128.jpg',
+      name: 'Edwin 1',
+      title: 'Customer Research Developer'
+    },
+    {
+      avatar: 'https://cdn.fakercloud.com/avatars/mwarkentin_128.jpg',
+      name: 'Edwin 2',
+      title: 'Customer Research Developer'
+    },
+    {
+      avatar: 'https://cdn.fakercloud.com/avatars/mwarkentin_128.jpg',
+      name: 'Edwin 3',
+      title: 'Customer Research Developer'
+    },
+  ]);
+
+  const buttonClasses = ['button']
+
+  if (cards.length < 3) buttonClasses.push('pink-button');
+  if (cards.length < 2) buttonClasses.push('red-button');
+
+  const toggleCard = () => setShowCard(!showCard);
+
+  const deleteCardHandler = (index)  => {
+    const cards_copy = [...cards];
+    cards_copy.splice(index, 1);
+    setCards(cards_copy)
+  }
+
+  const changeNameHandler = (event, index) => {
+    const cards_copy = [...cards];
+    cards_copy[index].name = event.target.value;
+    setCards(cards_copy);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={ theme }>
+      <div className="App">
+        <Button color="primary" length={ cards.length } >Show Hide</Button>
+        <button className={ buttonClasses.join(' ') } onClick={ () => toggleCard() } style={{ 'margin': '1rem 0' } }>Show / Hide card</button>
+        { showCard && (
+        cards.map(({ avatar, name, title }, index) => 
+          <Card key={ index }
+            avatar={ avatar }
+            name={ name }
+            title={ title }
+            onDelete={ () => deleteCardHandler(index) }
+            onNameChanges={ event =>  changeNameHandler(event, index)}
+          />)
+        )}
+      </div>
+    </ThemeProvider>
   );
 }
 
