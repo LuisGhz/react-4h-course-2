3ab5dba404985b5966f2c14e8c406691a17d6fa7
Luis Gutiérrez <luisghtz@outlook.com>
2021-06-01 21:29:41 -0500

    Delete image tag

--- src/card/index.js
+++ src/card/index.js
@@ -25,7 +25,6 @@ export const Card = props => {
 
   return (
     <div className="card">
-      <img src={`${ props.avatar }`} alt="Avatar" style={{ width: '100%' }} />
       <div className="container">
         <h4><b>{ props.name }</b></h4>
         <p>{ props.title }</p>
